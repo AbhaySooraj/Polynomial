2cc76b0a263338634804d6accbe672c28228010f
AbhaySooraj <94735651+AbhaySooraj@users.noreply.github.com>
2022-07-13 15:34:48 +0530

    Add files via upload

---
+++ poly.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int coefficient;
+	int power;
+	struct node *next;
+};
+
+struct node *p1=NULL,*p2=NULL,*p3=NULL;
+
+void cstruct(struct node *p,int size)
+{
+	int i;
+	for(i=size+1;i>0;i--)
+	{
+		printf("Enter coefficient of %d term :",i);
+		scanf("%d",&p->coefficient);
+		printf("Enter power of %d term :",i);
+		scanf("%d",&p->power);
+		p->next=(struct node*)malloc(sizeof(struct node));
+		p=p->next;
+		p->next =NULL;
+	}
+}
+
+void input(struct node *p)
+{
+	p3->power=p->power;
+	p3->coefficient=p->coefficient;
+	p=p->next;
+}
+
+void add(struct node *p1,struct node *p2,struct node *p3)
+{
+	while(p1->next && p2 ->next)
+	{	
+		if(p1->power>p2->power)
+		{	
+			input(p1);
+		}
+		
+		else if(p1->power>p2->power)
+		{
+			input(p2);
+		}
+		
+		else
+		{
+			p3->power=p1->power;
+			p3->coefficient=p1->coefficient + p2->coefficient;
+			p1=p1->next;
+			p2=p2->next;
+		}
+		
+		p3->next=(struct node *)malloc(sizeof(struct node));
+		p3=p3->next;
+		p3->next=NULL;
+	}
+	
+	while(p1->next || p2->next)
+	{
+		if(p1->next)
+		{
+			input(p1);
+		}
+		
+		if(p2->next)
+		{
+			input(p2);
+		}
+		
+		p3->next=(struct node *)malloc(sizeof(struct node));
+		p3=p3->next;
+		p3->next=NULL;
+	}
+}
+
+void display(struct node *p)
+{	
+		while(p->next!=NULL)
+		{
+			printf(" %dx^%d ",p->coefficient,p->power);
+			p=p->next;
+			if (p->next!=NULL)
+				printf("+");
+		}
+}	
+		
+void main()
+{
+	int x;
+	printf("Enter the highest power :");
+	scanf("%d",&x);
+	p1=(struct node*)malloc(sizeof(struct node));
+	p2=(struct node*)malloc(sizeof(struct node));
+	p3=(struct node*)malloc(sizeof(struct node));
+	
+	printf("\nPolynomial 1 \n");
+	cstruct(p1,x);
+	
+	printf("Polynomial 1:   ");
+	display(p1);
+	printf("\n");
+
+	printf("\nPolynomial 2 \n");
+	cstruct(p2,x);
+	printf("Polynomial 2:   ");
+	display(p2);
+	printf("\n");
+
+	add(p1,p2,p3); 
+	printf("\nThe sum is:   ");
+	display(p3); 
+	printf("\n");               		
+           
+}
